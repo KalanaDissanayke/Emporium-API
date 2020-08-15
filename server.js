4bb6279c8b0399dcc91b190cf3cb1f19fd7f12f2
Devin <devdhera@gmail.com>
2020-08-15 20:38:08 +0530

    feat(routes): base setup of routes
    adds products route base configuration to structure the route handling

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const dotenv = require('dotenv');
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Route files
+const products = require('./routes/products');
+
 const app = express();
+
+// Mount routers
+app.use('/api/v1/products', products);
 
 const PORT = process.env.PORT || 5000;
 
