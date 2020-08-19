7e4465a484269c647699ea1bc226240692d006a1
Devin <devdhera@gmail.com>
2020-08-20 03:11:52 +0530

    refactor(config): updates swagger config to match the current setup
    Removes swagger config from root and moves inside configs folder
    
    Adds servers to the
    swagger
    
    Changes apis setup to include wildcards

---
+++ config/swagger.conf.js
@@ -0,0 +1,32 @@
+const options = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Emporium API',
+            version: '1.0.0',
+            description: 'Best marketplace for introverts 🤓',
+            license: {
+                name: 'MIT',
+                url: '',
+            },
+        },
+        servers: [
+            {
+                url: 'http://localhost:{port}/{basePath}',
+                description: 'Development Server',
+                variables: {
+                    port: {
+                        enum: ['5000'],
+                        default: '5000',
+                    },
+                    basePath: {
+                        default: 'api/v1',
+                    },
+                },
+            },
+        ],
+    },
+    apis: ['./routes/*.js', './models/*.js'],
+};
+
+module.exports = options;
