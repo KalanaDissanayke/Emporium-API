30b84bfcd7e01db54be6d3b47d810d7f84c62976
Devin <devdhera@gmail.com>
2020-08-24 21:37:27 +0530

    refactor(controllers): refactores products contoller to use async handler

--- controllers/products.js
+++ controllers/products.js
@@ -1,79 +1,60 @@
 const Product = require('../models/Product');
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 
 // @desc            Get all products
 // @route           GET /api/v1/products
 // @access          Public
-exports.getProducts = async (req, res, next) => {
-    try {
-        const products = await Product.find();
+exports.getProducts = asyncHandler(async (req, res, next) => {
+    const products = await Product.find();
 
-        res.status(200).json({ success: true, count: products.length, data: products });
-    } catch (error) {
-        next(error);
-    }
-};
+    res.status(200).json({ success: true, count: products.length, data: products });
+});
 
 // @desc            Get single products
 // @route           GET /api/v1/products/:id
 // @access          Public
-exports.getProduct = async (req, res, next) => {
-    try {
-        const product = await Product.findById(req.params.id);
+exports.getProduct = asyncHandler(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
 
-        if (!product) {
-            return next(new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404));
-        }
-
-        res.status(200).json({ success: true, data: product });
-    } catch (error) {
-        next(error);
+    if (!product) {
+        return next(new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404));
     }
-};
+
+    res.status(200).json({ success: true, data: product });
+});
 
 // @desc            Create new product
 // @route           POST /api/v1/products
 // @access          Private
-exports.createProduct = async (req, res, next) => {
-    try {
-        const product = await Product.create(req.body);
+exports.createProduct = asyncHandler(async (req, res, next) => {
+    const product = await Product.create(req.body);
 
-        res.status(201).json({ success: true, data: product });
-    } catch (error) {
-        next(error);
-    }
-};
+    res.status(201).json({ success: true, data: product });
+});
 
 // @desc            Update product
 // @route           PUT /api/v1/products/:id
 // @access          Private
-exports.updateProduct = async (req, res, next) => {
-    try {
-        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+exports.updateProduct = asyncHandler(async (req, res, next) => {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
 
-        if (!product) {
-            return next(new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404));
-        }
-
-        res.status(200).json({ success: true, data: product });
-    } catch (error) {
-        next(error);
+    if (!product) {
+        return next(new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404));
     }
-};
+
+    res.status(200).json({ success: true, data: product });
+});
 
 // @desc            Delete product
 // @route           DELETE /api/v1/products/:id
 // @access          Private
-exports.deleteProduct = async (req, res, next) => {
-    try {
-        const product = await Product.findByIdAndDelete(req.params.id);
+exports.deleteProduct = asyncHandler(async (req, res, next) => {
+    const product = await Product.findByIdAndDelete(req.params.id);
 
-        if (!product) {
-            return next(new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404));
-        }
-
-        res.status(200).json({ success: true, data: {} });
-    } catch (error) {
-        next(error);
+    if (!product) {
+        return next(new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404));
     }
-};
+
+    res.status(200).json({ success: true, data: {} });
+});
