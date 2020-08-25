ebc542ec7f5d79c1361b8981ebb6f3c4fa13a513
Devin <devdhera@gmail.com>
2020-08-25 12:18:10 +0530

    feat(routes/categories): adds GET single category route

--- routes/categories.js
+++ routes/categories.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getCategories } = require('../controllers/categories');
+const { getCategories, getCategory, createCategory } = require('../controllers/categories');
 
 // Include other resource routers
 const productsRouter = require('./products');
@@ -9,6 +9,8 @@ const router = express.Router();
 // Re-route into other resource routers
 router.use('/:categoryId/products', productsRouter);
 
-router.route('/').get(getCategories);
+router.route('/').get(getCategories).post(createCategory);
+
+router.route('/:id').get(getCategory);
 
 module.exports = router;
