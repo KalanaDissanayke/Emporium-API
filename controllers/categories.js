4a889e78b2623c69cab2ae61e5ce108ba5ef6dd0
Devin <devdhera@gmail.com>
2020-08-25 12:17:11 +0530

    feat(controllers/products): adds GET single category route

--- controllers/categories.js
+++ controllers/categories.js
@@ -14,3 +14,25 @@ exports.getCategories = asyncHandler(async (req, res, next) => {
 
     res.status(200).json({ success: true, count: categories.length, data: categories });
 });
+
+// @desc            Get single category
+// @route           GET /api/v1/categories/:id
+// @access          Public
+exports.getCategory = asyncHandler(async (req, res, next) => {
+    const category = await Category.findById(req.params.id).populate('products');
+
+    if (!category) {
+        return next(new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404));
+    }
+
+    res.status(200).json({ success: true, data: category });
+});
+
+// @desc            Create new category
+// @route           POST /api/v1/categories
+// @access          Private
+exports.createCategory = asyncHandler(async (req, res, next) => {
+    const category = await Category.create(req.body);
+
+    res.status(201).json({ success: true, data: category });
+});
