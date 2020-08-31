4ca17ef85e6779ac36863e80313cce57a8ec29d7
Devin <devdhera@gmail.com>
2020-08-31 20:38:39 +0530

    feat(models/review): aggegrate average rating for a product

--- models/Review.js
+++ models/Review.js
@@ -14,8 +14,8 @@ const ReviewSchema = new mongoose.Schema({
     rating: {
         type: Number,
         min: 1,
-        max: 10,
-        required: [true, 'Please add a rating between 1 and 10'],
+        max: 5,
+        required: [true, 'Please add a rating between 1 and 5'],
     },
     createdAt: {
         type: Date,
@@ -35,5 +35,38 @@ const ReviewSchema = new mongoose.Schema({
 
 // Prevent user from submitting more than one review per product
 ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+// Static method to get avg rating and save
+ReviewSchema.statics.getAverageRating = async function (productId) {
+    const obj = await this.aggregate([
+        {
+            $match: { product: productId },
+        },
+        {
+            $group: {
+                _id: '$product',
+                averageRating: { $avg: '$rating' },
+            },
+        },
+    ]);
+
+    try {
+        await this.model('Product').findByIdAndUpdate(productId, {
+            averageRating: obj[0].averageRating,
+        });
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+// Call getAverageCost after save
+ReviewSchema.post('save', async function () {
+    await this.constructor.getAverageRating(this.product);
+});
+
+// Call getAverageCost before remove
+ReviewSchema.post('remove', async function () {
+    await this.constructor.getAverageRating(this.product);
+});
 
 module.exports = mongoose.model('Review', ReviewSchema);
