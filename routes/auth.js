88775faf6df1f197418986097b1461e3f8fb1cb5
Devin <devdhera@gmail.com>
2020-08-31 18:38:29 +0530

    feat(controllers/auth): adds user password update route

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,13 @@
 const express = require('express');
-const { register, login, getMe, forgotPassword, resetPassword, updateDetails } = require('../controllers/auth');
+const {
+    register,
+    login,
+    getMe,
+    forgotPassword,
+    resetPassword,
+    updateDetails,
+    updatePassword,
+} = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
@@ -77,6 +85,8 @@ router.route('/me');
 router.get('/me', protect, getMe);
 
 router.put('/updatedetails', protect, updateDetails);
+
+router.put('/updatepassword', protect, updatePassword);
 
 router.post('/forgotpassword', forgotPassword);
 
